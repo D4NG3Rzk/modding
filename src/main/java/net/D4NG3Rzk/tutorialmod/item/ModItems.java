2a617dbf1ff90e326ac9e66b18367c8e525774c3
D4NG3Rzk <122655906+D4NG3Rzk@users.noreply.github.com>
2023-01-14 00:10:53 -0300

    Add Items

---
+++ src/main/java/net/D4NG3Rzk/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.D4NG3Rzk.tutorialmod.item;
+
+import net.D4NG3Rzk.tutorialmod.TutorialMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> OBSIDIAN_WHITE_FRAGMENT = ITEMS.register("obsidian_white_fragment",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> JARRON = ITEMS.register("jarron",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> JARRONCUM = ITEMS.register("jarroncum",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
